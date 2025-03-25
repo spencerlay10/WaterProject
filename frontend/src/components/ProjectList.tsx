03e0146a23d38c099f9b941595a413b4353dd0ab
spencerlay10 <sklay10@gmail.com>
2025-03-24 19:04:50 -0600

    finalize

--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { Project } from "./types/Project";
+import { Project } from "../types/Project";
 import CookieConsent from "react-cookie-consent";
+import { useNavigate } from "react-router-dom";
 
 function ProjectList({ selectedCategories }: { selectedCategories: string[] }) {
   const [projects, setProjects] = useState<Project[]>([]);
@@ -8,6 +9,7 @@ function ProjectList({ selectedCategories }: { selectedCategories: string[] }) {
   const [pageNum, setPageNum] = useState<number>(1);
   const [totalItems, setTotalItems] = useState<number>(0);
   const [totalPages, setTotalPages] = useState<number>(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -58,6 +60,13 @@ function ProjectList({ selectedCategories }: { selectedCategories: string[] }) {
                 {p.projectFunctionalityStatus}
               </li>
             </ul>
+
+            <button
+              className="btn btn-success"
+              onClick={() => navigate(`/donate/${p.projectName}/${p.projectId}`)}
+            >
+              Donate
+            </button>
           </div>
         </div>
       ))}
---
+++ frontend/src/pages/ProjectsPage.tsx
@@ -0,0 +1,35 @@
+import CategoryFilter from "../components/CategoryFilter";
+import ProjectList from "../components/ProjectList";
+import WelcomeBand from "../components/WelcomeBand";
+import "../App.css";
+import { useState } from "react";
+import CartSummary from "../components/CartSummary";
+
+function ProjectsPage() {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  return (
+    <>
+      <div className="container mt-4">
+        <CartSummary />
+        <WelcomeBand />
+        <div className="row">
+          <div className="col-md-3">
+            <CategoryFilter
+              selectedCategories={selectedCategories}
+              setSelectedCategories={setSelectedCategories}
+            />
+          </div>
+          <div className="col-md-9">
+            <ProjectList selectedCategories={selectedCategories} />
+          </div>
+        </div>
+      </div>
+      {/* <CookieConsent>
+        This website uses cookies to enhance the user experience.
+      </CookieConsent> */}
+    </>
+  );
+}
+
+export default ProjectsPage;
